ace792bc30a334b7d084b51a3bbaa1b28f85fb49
ngzhenghui94 <ngzhenghui94@hotmail.com>
2022-03-08 22:00:37 +0800

    added self-test

---
+++ test.py
@@ -0,0 +1,12 @@
+from main import doesMsgContainEthAddr
+
+x = []
+x.append(doesMsgContainEthAddr("0x0000000000000000000000000000000000000000"))
+x.append(doesMsgContainEthAddr("0xccA80041F6641d790D8198B41af487ad344dE1F8"))
+x.append(doesMsgContainEthAddr("0x0ccAF8cB1C92aef64dD36ce1f3882D195180AD5C"))
+x.append(doesMsgContainEthAddr("0x573B66B0eD239492185714e57C7A925a396ee0F6"))
+x.append(doesMsgContainEthAddr("0x573B66B0eD239492185714e57C7A925a396ee0F6"))
+x.append(doesMsgContainEthAddr("0x573B66B0eD239492185714e57C7A925a396ee0F6"))
+
+print(x)
+
